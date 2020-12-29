b5788784b0721b2bdb6b50f35563da1d2d2cedc1
Muhd Hanis Harun <muhdhanis08@gmail.com>
2020-12-29 15:59:12 +0800

    add model for table peribadi, kodsesi, thnsempl

---
+++ app/Models/StudentSemester.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StudentSemester extends Model
+{
+    use HasFactory;
+    protected $table = 'KRS_ThnSemPL';
+}
